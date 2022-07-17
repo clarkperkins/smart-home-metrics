21c4dadadfe2ad00635ce24994c6eb4c1fda1d40
Clark Perkins <r.clark.perkins@gmail.com>
2022-07-17 15:08:34 -0500

    Run the prometheus server with asgi

--- shm/cli.py
+++ shm/cli.py
@@ -1,17 +1,9 @@
 import asyncio
-import logging
-import logging.config
-from pathlib import Path
 
 import click
-import yaml
-from prometheus_client import start_http_server
 
-from shm.metrics import collect_metrics
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-logger = logging.getLogger(__name__)
+from shm.logging import setup_logging
+from shm.metrics import Collector
 
 
 @click.group(name="shm")
@@ -23,16 +15,9 @@ def main():
 @click.option("--address", default="0.0.0.0")
 @click.option("--port", "-p", type=int, default=9000)
 def run(address: str, port: int):
-    logging_config_file = BASE_DIR / "logging.yaml"
-    if logging_config_file.is_file():
-        with logging_config_file.open("rt", encoding="utf8") as f:
-            logging.config.dictConfig(yaml.safe_load(f))
-
-    start_http_server(port=port, addr=address)
-
-    logger.info("Started prometheus metrics server at %s:%d", address, port)
-
-    asyncio.run(collect_metrics())
+    setup_logging()
+    collector = Collector(address, port)
+    asyncio.run(collector.run())
 
 
 if __name__ == "__main__":
